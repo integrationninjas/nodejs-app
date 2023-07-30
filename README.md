a75ca313e3008161285ba7fc5627a0c71752c532
integrationninjas <140945507+integrationninjas@users.noreply.github.com>
2023-07-30 14:43:55 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # graphql-sample-app
-GraphQL Sample Application
+
+An example of GraphQL queries/mutations with Node and Express js.
+
+With GraphQL, clients can specify exactly what data they need, and the server responds with only that data, reducing the amount of data transferred over the network.
+
+Rest API Endpoint for get all users: http://localhost:5000/rest/getAllUsers
+
+GraphQL Endpont: http://localhost:5000/graphql
+
+Query for below scenarios: 
+
+1. Get All Users with query operation
+
+query{
+  getAllUsers{
+    id
+    email
+  }
+}
+
+2. Get single user details
+
+query{
+  findUserById(id:1000){
+    id
+    firstName
+    lastName
+    email
+  }
+}
+
+3. Create User with mutation operation
+
+mutation{
+  createUser(firstName:"sachin",lastName:"purohit",email:"sachin@sachin.com",password:"password"){
+    id
+    firstName
+    lastName
+    email
+  }
+}
+
---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const userData = require("./MOCK_DATA.json");
+const graphql = require("graphql")
+const { GraphQLObjectType, GraphQLSchema, GraphQLList, GraphQLID, GraphQLInt, GraphQLString } = graphql
+const { graphqlHTTP } = require("express-graphql")
+
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: () => ({
+        id: { type: GraphQLInt },
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        getAllUsers: {
+            type: new GraphQLList(UserType),
+            args: { id: {type: GraphQLInt}},
+            resolve(parent, args) {
+                return userData;
+            }
+        },
+        findUserById: {
+            type: UserType,
+            description: "fetch single user",
+            args: { id: {type: GraphQLInt}},
+            resolve(parent, args) {
+                return userData.find((a) => a.id == args.id);
+            }
+        }
+    }
+})
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        createUser: {
+            type: UserType,
+            args: {
+                firstName: {type: GraphQLString},
+                lastName: { type: GraphQLString },
+                email: { type: GraphQLString },
+                password: { type: GraphQLString },
+            },
+            resolve(parent, args) {
+                userData.push({
+                    id: userData.length + 1,
+                    firstName: args.firstName,
+                    lastName: args.lastName,
+                    email: args.email,
+                    password: args.password
+                })
+                return args
+            }
+        }
+    }
+})
+
+const schema = new GraphQLSchema({query: RootQuery, mutation: Mutation})
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.get("/rest/getAllUsers", (req, res) => {
+    res.send(userData)
+   });
+
+app.listen(PORT, () => {
+  console.log("Server running");
+});
