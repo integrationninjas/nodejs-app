e29056a298081e7b6f717394265b8793c9c16122
integrationninjas <integrationninjas@gmail.com>
2023-12-22 23:10:21 +0530

    update port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = 8080;
 const userData = require("./MOCK_DATA.json");
 const graphql = require("graphql")
 const { GraphQLObjectType, GraphQLSchema, GraphQLList, GraphQLID, GraphQLInt, GraphQLString } = graphql
