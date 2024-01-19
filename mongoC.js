68ead5144f94540885354638d7819d975e804701
integrationninjas <integrationninjas@gmail.com>
2024-01-20 00:10:33 +0530

    test node deplo ec2

---
+++ mongoC.js
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb";
+
+const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.ft5jq9v.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const client = new MongoClient(connectionString);
+let conn;
+try {
+  conn = await client.connect();
+  console.log("connection successful")
+} catch(e) {
+  console.error(e);
+}
+let db = conn.db("integration_ninjas");
+export default db;
