d0ebb03aa8ee03057146e22b3f0c5a5d18a3c666
E. P. <epo.datascience@gmail.com>
2024-02-20 16:45:19 +0100

    deploy to ec2

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,8 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://epodatascience:${password}@devcluster.8mcy1vw.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+// mongodb+srv://epodatascience:<password>@devcluster.8mcy1vw.mongodb.net/?retryWrites=true&w=majority
 const client = new MongoClient(connectionString);
 let conn;
 try {
