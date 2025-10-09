299b2134ffab2d31923abffd63c2d18dfec9f152
Kunal1522 <kunalkashyap8024@gmail.com>
2025-10-09 13:49:27 +0000

    Updated CICD workflow for DockerHub deployment

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+    res.send('Hello World, from Kunal');
 })
 
 app.post('/addUser',async (req, res) => {
